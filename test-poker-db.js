925a94a1f53a9263e39a128086458679003f4ea4
Hugo ter Doest <hwl.ter.doest@gmail.com>
2015-08-31 22:55:03 +0200

    Split the database in test and training set and did some tests with the naive similarity calculation.

--- test-poker-db.js
+++ test-poker-db.js
@@ -25,12 +25,24 @@ var FLOP = 1;
 var TURN = 2;
 var RIVER = 3;
 
-/**
- * @return {number}
- */
+var sizeOfTrainingSet = 0.7;
+var trainingSet = [];
+var testingSet = [];
+
+function splitIntoTestAndTrainSet() {
+  classContextPairs.forEach(function(pair) {
+    if (Math.random() < sizeOfTrainingSet) {
+      trainingSet.push(pair);
+    }
+    else {
+      testingSet.push(pair);
+    }
+  });
+}
+
 // context1 is a context available in the database
-// context2 is a context that should be classified, it may be icncomplete
-function Similarity(context1, context2) {
+// context2 is a context that should be classified, it may be incomplete
+function computeSimilarity(context1, context2) {
   var similarity = 0;
   // Compare number of players
   if (context2.numberOfPlayers) {
@@ -42,70 +54,116 @@ function Similarity(context1, context2) {
   // Compare number of active players
   if (context2.numberOfActivePlayers) {
     if (context1.numberOfActivePlayers === context2.numberOfActivePlayers) {
-      similarity++;
+      similarity += 1;
     }
   }
 
   // Compare player position
   if (context2.playerPosition) {
-    if (context1.numberOfPlayers === context2.numberOfPlayers) {
-      similarity++;
+    if (context1.playerPosition === context2.playerPosition) {
+      similarity += 1;
     }
   }
 
   // Compare pot before betting action
   if (context2.potBeforeBettingAction) {
     // Test whether pots are in the same magnitude
-    if (math.round(math.log(context1.potBeforeBettingAction, 10)) === math.round(math.log(context2.pot, 10))) {
-      similarity++;
+    if (math.round(math.log(context1.potBeforeBettingAction, 10)) ===
+      math.round(math.log(context2.potBeforeBettingAction, 10))) {
+      //similarity++;
     }
   }
 
   // Compare betting round
-  if (context2.potBeforeBettingAction) {
+  if (context2.bettingRound) {
     if (context1.bettingRound === context2.bettingRound) {
-      similarity++;
+      //similarity += 1;
     }
   }
 
+  var maxIndex = -1;
   // Compare community cards
-  context2.communityCards.forEach(function(card) {
-    if (context1.communityCards.indexOf(card) > -1) {
-      similarity++;
+  switch (context2.bettingRound) {
+    case PREFLOP:
+      maxIndex = -1;
+      break;
+    case FLOP:
+      maxIndex = 2;
+      break;
+    case TURN:
+      maxIndex = 3;
+      break;
+    case RIVER:
+      maxIndex = 4;
+      break;
+  }
+  context2.communityCards.forEach(function(card, index) {
+    // Look at cards that are known in this round
+    if (index <= maxIndex) {
+      var indexOfCard = context1.communityCards.indexOf(card);
+      if ((indexOfCard !== -1) && (indexOfCard <= maxIndex)) {
+        similarity += 1;
+      }
     }
   });
+
   return(similarity);
 }
 
-function classifyHand(holeCard1, holeCard2, bettingRound, communityCards, pot, numberOfPlayers) {
+function createContext(holeCards, bettingRound, communityCards, potBeforeBettingAction, numberOfPlayers) {
+  var context = {};
+
+  context.holeCards = holeCards;
+  context.bettingRound = bettingRound;
+  context.communityCards = communityCards;
+  context.potBeforeBettingAction = potBeforeBettingAction;
+  context.numberOfPlayers = numberOfPlayers;
+
+  return(context);
+}
+
+function equalHoleCards(holeCards1, holeCards2) {
+ return(
+   ((holeCards1.indexOf(holeCards2[0]) > -1) &&
+  (holeCards1.indexOf(holeCards2[1]) > -1)) ? 1 : 0
+ );
+}
+
+function similarHoleCards(holeCards1, holeCards2) {
+  return(
+    (((holeCards1[0][0] === holeCards2[0][0]) &&
+    (holeCards1[1][0] === holeCards2[1][0])) ||
+    ((holeCards2[0][0] === holeCards1[1][0]) &&
+    (holeCards2[0][0] === holeCards1[1][0]))) ? 1 : 0
+  );
+}
+
+function suitedHoleCards(holeCards1, holeCards2) {
+  return (
+    ((holeCards1[0][1] === holeCards1[1][1]) &&
+    (holeCards2[0][1] === holeCards2[1][1])) ? 1 : 0
+  );
+}
+
+function classifyContext(contextToBeClassified) {
   // Find class context pairs for these hole card
   var matches = [];
-  classContextPairs.forEach(function(pair) {
-    if ((pair.context.holeCards.indexOf(holeCard1) > -1) && (pair.context.holeCards.indexOf(holeCard2) > -1)) {
+  trainingSet.forEach(function(pair) {
+    var similarity = equalHoleCards(contextToBeClassified.holeCards, pair.context.holeCards);
+    //similarity += suitedHoleCards(contextToBeClassified.holeCards,
+    // pair.context.holeCards);
+    similarity += similarHoleCards(contextToBeClassified.holeCards, pair.context.holeCards);
+    if (similarity) {
+      pair.similarity = similarity;
       matches.push(pair);
     }
   });
-  console.log('classifyHand: number of matching betting actions based on' +
-    ' hole cards: ' + matches.length);
+  //console.log('classifyHand: number of matching betting actions based on' +
+  //  ' hole cards: ' + matches.length);
   // Score the matched context
   matches.forEach(function(pair) {
-    pair.similarity = 0;
-    // Test whether pots are in the same magnitude
-    if (math.round(math.log(pair.context.potBeforeBettingAction, 10)) === math.round(math.log(pot, 10))) {
-      pair.similarity++;
-    }
-    if (pair.bettingRound === bettingRound) {
-      pair.similarity++;
-    }
-    if (math.abs(numberOfPlayers - pair.context.numberOfPlayers) < 3) {
-      pair.similarity++;
-    }
-    communityCards.forEach(function(card) {
-      if (pair.context.communityCards.indexOf(card) > -1) {
-        pair.similarity++;
-      }
-    });
-    console.log(pair.similarity);
+    pair.similarity += computeSimilarity(pair.context, contextToBeClassified);
+    //console.log(pair.similarity);
   });
   // Sort descending by similarity
   matches.sort(function(a, b) {
@@ -115,5 +173,23 @@ function classifyHand(holeCard1, holeCard2, bettingRound, communityCards, pot, n
   return(matches[0]);
 }
 
-var action = classifyHand('Jd', '7h', FLOP, ['2h', 'Kh', '8c'], 150, 7);
-console.log('Best matching betting action: ' + JSON.stringify(action, null, 2));
+function testSimilarityMeasure() {
+  var recallBettingAction = 0;
+  var recallBettingActionPlusAmount = 0;
+  var numberOfTestsProcessed = 0;
+  splitIntoTestAndTrainSet();
+  testingSet.forEach(function(pair) {
+    var classification = classifyContext(pair.context);
+    if (classification.class.bettingAction === pair.class.bettingAction) {
+      recallBettingAction++;
+      if (classification.class.bettingAmount === pair.class.bettingAmount) {
+        recallBettingActionPlusAmount++;
+      }
+    }
+    numberOfTestsProcessed++;
+    console.log('Recall betting actions: ' + math.round(100 * recallBettingAction / numberOfTestsProcessed) + ' (' + numberOfTestsProcessed + ' tests processed)');
+    console.log('Recall betting actions including betting amounts: ' + math.round(100 * recallBettingActionPlusAmount / numberOfTestsProcessed));
+  });
+}
+
+testSimilarityMeasure();
